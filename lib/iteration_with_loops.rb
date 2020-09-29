ee0dba87b9966cfb669b9b33aab2aaeecfa3b3c6
convivial-parser-7775 <avelonpang@gmail.com>
2020-09-29 04:32:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,12 +8,13 @@ def join_nested_strings(src)
     element_index = 0 
     
     while element_index < src[row_index].count do 
-      if src[row_index][element_index].class == String 
-         #new_array << src[row_index][element_index] 
+      if src[row_index][element_index].instance_of? String
+        #src[row_index][element_index].class == String 
+         new_array << src[row_index][element_index] 
   end
   element_index += 1 
 end
-new_array << src[row_index][element_index] 
+#new_array << src[row_index][element_index] 
 row_index += 1 
 end 
 new_array 
